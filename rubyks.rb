5ba3abfc3a56e5194bd09dae16ed92d08d284e1a
urthbound <jeffowler@gmail.com>
2013-11-13 12:05:16 -0500

    onclick working

--- rubyks.rb
+++ rubyks.rb
@@ -1,30 +1,12 @@
 require 'sinatra'
 require 'rubyks'
-
 require_relative 'helpers/cubehelpers.rb'
 
-#before do
-#  if @cube == nil
-#    @cube = Cube.new
-#    @cube.cube = state_to_array(params[:state])
-#  end
-#end
-#
 get("/") do
-  if @cube == nil
-    @cube = Cube.new
-    @cube.cube = state_to_array(params[:state])
-  end
-
+  @cube = Cube.new
   erb :index
 end
 
-get("/scramble") do
-  @cube.scramble
-  redirect "/  "
-end
-
 post("/u") do
-  @cube.u
-  redirect "/"
+  params[:state]
 end
