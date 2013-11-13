3ffff9edd604a921e1c56b88063f213fc2b9aa3c
urthbound <jeffowler@gmail.com>
2013-11-12 23:44:02 -0500

    ooooo

--- rubyks.rb
+++ rubyks.rb
@@ -3,22 +3,28 @@ require 'rubyks'
 
 require_relative 'helpers/cubehelpers.rb'
 
-before do
-  @cube = Cube.new
-  @cube.cube = state_to_array(params[:state])
-end
-
+#before do
+#  if @cube == nil
+#    @cube = Cube.new
+#    @cube.cube = state_to_array(params[:state])
+#  end
+#end
+#
 get("/") do
+  if @cube == nil
+    @cube = Cube.new
+    @cube.cube = state_to_array(params[:state])
+  end
+
   erb :index
 end
 
 get("/scramble") do
   @cube.scramble
-  redirect "/?state=#{@cube.cube.join.to_s}"
+  redirect "/  "
 end
 
-get("/u") do
+post("/u") do
   @cube.u
-  redirect "/?state=#{@cube.cube.join.to_s}"
+  redirect "/"
 end
-
