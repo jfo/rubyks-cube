b2796aecf15e90ab15e191e876bfc5a1b7357ca1
urthbound <jeffowler@gmail.com>
2013-11-13 15:54:10 -0500

    added returns from new cube state for post requests

--- rubyks.rb
+++ rubyks.rb
@@ -13,28 +13,35 @@ end
 
 post("/u") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.u.cube)
 end
 
 post("/d") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.d.cube)
 end
 
 post("/l") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.l.cube)
 end
 
 post("/r") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.r.cube)
 end
 
 post("/b") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.d.cube)
 end
 
 post("/f") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.f.cube)
 end
 
 post("/solve") do
   params[:state] #is the json state array
+  json html_colorize(Cube.new.cube)
 end
