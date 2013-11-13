6eff645d928d8f4fc232d11e9476436bafcaf665
urthbound <jeffowler@gmail.com>
2013-11-13 14:53:49 -0500

    scramble communicates using json and updates styles without any erb at all

--- rubyks.rb
+++ rubyks.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'rubyks'
+require 'sinatra/json'
 require_relative 'helpers/cubehelpers.rb'
 
 get("/") do
-  @cube = Cube.new
   erb :index
 end
 
-post("/test") do
-  return "blue"
+post("/scramble") do
+  json html_colorize(Cube.new.scramble.cube)
 end
